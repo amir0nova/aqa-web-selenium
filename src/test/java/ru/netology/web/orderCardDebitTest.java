43d9dfa76c7e7dd1329bf6bbe7308b0e6780ea53
Alexandra Mironova <sominka1939@gmail.com>
2020-03-31 17:10:02 +0300

    (c)orderCardDebitTest: changed \\ to /

--- src/test/java/ru/netology/web/orderCardDebitTest.java
+++ src/test/java/ru/netology/web/orderCardDebitTest.java
@@ -17,7 +17,7 @@ public class orderCardDebitTest {
     private WebDriver driver;
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver\\win\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "driver/win/chromedriver.exe");
     }
     @BeforeEach
     void setUp() {
