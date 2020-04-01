6ccbc3038957c5b24bdf2861b6abb09b5feabf79
Alexandra Mironova <sominka1939@gmail.com>
2020-04-01 23:38:46 +0300

    (c)orderCardDebitTest: added some testing methods

--- src/test/java/ru/netology/web/orderCardDebitTest.java
+++ src/test/java/ru/netology/web/orderCardDebitTest.java
@@ -34,6 +34,7 @@ public class orderCardDebitTest {
         driver = null;
     }
 
+    // number 1
     @Test
     void shouldFormFilledCorrectlyWithClassName() {
         driver.get("http://localhost:9999");
@@ -44,5 +45,58 @@ public class orderCardDebitTest {
         driver.findElement(By.className("button_theme_alfa-on-white")).click();
         String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    // number 2
+    @Test
+    void shouldFormFilledSurnameNameHyphenNameWithClassName() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Германова Анна-Мария");
+        elements.get(1).sendKeys("+79105552233");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    // number 3
+    @Test
+    void shouldFormFilledSurnameDoubleNameWithClassName() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Муранов Петр Василий");
+        elements.get(1).sendKeys("+78002000539");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    // number 4
+    @Test
+    void shouldFormFilledSurnameForeHyphensAndNameWithClassName() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Пабло Диего Хозе Франциско Паула Петр");
+        elements.get(1).sendKeys("+74998652351");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.className("paragraph_theme_alfa-on-white")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    // Задача №2 - Проверка валидации (необязательная)
+
+    @Test
+    void shouldSurnameAndNameFilledRussianWithClassName() {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Fedorova Maria");
+        elements.get(1).sendKeys("+79252548652");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
+        String text = driver.findElement(By.className("input__sub")).getText();
+        assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
 }
